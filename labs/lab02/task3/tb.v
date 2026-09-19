714143f5186222aa23bca8532cd9e8dbcf603987
Yatharth Gupta <yathartgpta@gmail.com>
2026-09-19 12:35:19 +0000

    lab02 task3

--- labs/lab02/task3/tb.v
+++ labs/lab02/task3/tb.v
@@ -1,6 +1,7 @@
 module tb;
   reg [1:0] t_A; reg [1:0] t_B;
   wire t_GT; wire t_LT; wire t_EQ;
+  reg t_exp_GT; reg t_exp_LT; reg t_exp_EQ;
 
   comp2 DUT (
     .A (t_A),
@@ -19,13 +20,26 @@ module tb;
     end
   end
 
-  initial begin
-    t_A = 2'd0; t_B = 2'd1;
-    #5 t_A = 2'd3; t_B = 2'd2;
-    #5 t_A = 2'd1; t_B = 2'd1;
-  end
+  integer i, j, errors;
 
-  initial
-    $monitor($time, " A=%b B=%b | GT=%b LT=%b EQ=%b", t_A, t_B, t_GT, t_LT, t_EQ); // change as required
+  initial begin
+    errors = 0;
+    for (i = 0; i < 4; i = i + 1) begin
+      for (j = 0; j < 4; j = j + 1) begin
+        t_A = i; t_B = j;
+        #5;
+
+        t_exp_GT = t_A > t_B; t_exp_LT = t_A < t_B; t_exp_EQ = t_A == t_B;
+        if ({t_exp_GT, t_exp_LT, t_exp_EQ} !== {t_GT, t_LT, t_EQ}) begin
+          $display("FAIL: A=%b B=%b | got GT=%b LT=%b EQ=%b, expected GT=%b LT=%b EQ=%b", t_A, t_B, t_GT, t_LT, t_EQ, t_exp_GT, t_exp_LT, t_exp_EQ);
+          errors = errors + 1;
+        end
+      end
+    end
+
+    if (errors == 0) $display("PASS: all cases correct");
+    else $display("FAIL: %0d errors", errors);
+    $finish;
+  end
 
 endmodule
